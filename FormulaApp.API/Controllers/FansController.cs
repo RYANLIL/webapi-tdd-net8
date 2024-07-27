86c216aa599395a636fa348a9f912abf23ecf96c
RyanL <RYANLIL@github.com>
2024-07-26 21:39:55 -0400

    Add first test Update FormulaApp.Test.csproj for test control in visual studio

---
+++ FormulaApp.API/Controllers/FansController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormulaApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FansController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetFans()
+        {
+            return Ok("Fans"); //returns HttpStatusCode 200
+        }
+    }
+}
---
+++ FormulaApp.Test/Systems/Controllers/TestFansControllers.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using FormulaApp.API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaApp.Test.Systems.Controllers
+{
+    public class TestFansControllers
+    {
+        [Fact]
+        public async Task Get_OnSuccess_ReturnsStatusCode_200()
+        {
+            // Arrange What the test needs to run
+            var fansController = new FansController();
+
+            // Act
+            var result = (OkObjectResult) await fansController.GetFans();
+
+
+            // Assert
+
+            result.StatusCode.Should().Be(200);
+        }
+
+    }
+}
