d69096f09d81d935b429537c4804a8a979333a01
RyanL <RYANLIL@github.com>
2024-07-27 00:25:22 -0400

    Implement get all fans in FanService

--- FormulaApp.API/Services/FanService.cs
+++ FormulaApp.API/Services/FanService.cs
@@ -1,18 +1,40 @@
 ﻿using FormulaApp.API.Models;
 using FormulaApp.API.Services.Interfaces;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace FormulaApp.API.Services
 {
     public class FanService : IFanService
     {
         private readonly HttpClient _httpClient;
-        public FanService(HttpClient httpClient) { 
+        public FanService(HttpClient httpClient) {
             _httpClient = httpClient;
         }
 
         public async Task<List<Fan>> GetAllFans()
         {
-            throw new NotImplementedException();
+            var fans = new List<Fan>
+                {
+                    new Fan()
+                    {
+                        Id = 1,
+                        Name = "Test",
+                        Email = "test@email.com"
+                    },
+                    new Fan()
+                    {
+                        Id = 2,
+                        Name = "don",
+                        Email = "don.donne@email.com"
+                    },
+                };
+
+            return fans;
         }
+
+        
+
     }
 }
+
